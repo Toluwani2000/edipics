780ba38fc5f15d712347fb2e9726bc46e830741f
Toluwani <toluadtech@gmail.com>
2023-04-07 16:18:23 +0100

    Finished Functionalities

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -47,7 +47,7 @@ const Modal = ({ showModal, setShowModal }: Props) => {
                         id="email"
                         readOnly
                         className=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                        value={showModal.image.links.download}
+                        value={showModal.image.src.original}
                         required
                       />
                     </div>
